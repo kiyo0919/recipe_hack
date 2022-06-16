44caa6a0e7597f16c96eca1294fabc99a9b40567
kiyo0919 <83300871+kiyo0919@users.noreply.github.com>
2022-06-16 23:18:40 +0900

    heroku

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,16 +1,3 @@
 const { environment } = require('@rails/webpacker')
 
 module.exports = environment
-<<<<<<< HEAD
-=======
-
-const webpack = require('webpack')
-environment.plugins.prepend(
-  'Provide',
-  new webpack.ProvidePlugin({
-    $: 'jquery/src/jquery',
-    jQuery: 'jquery/src/jquery',
-    Popper: 'popper.js'
-  })
-)
->>>>>>> b87a0b962c61c4a0e7cf1b80daaa35a05a8a1fda
